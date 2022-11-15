0718d6811b7588836a617adaff65c6b83064295c
mumarejaz <umar.ejaz@devsinc.com>
2022-11-15 13:44:30 +0500

    added .env and heroku config

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
-const Item = require("./models/itemModel.js");
+require("dotenv").config()
+const path = require("path")
 const db = require("./db.js");
 const app = express();
 const itemsRoute = require("./routes/itemsRoute.js");
@@ -10,10 +11,18 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.send("Server working...");
 });
+
 app.use("/api/users/", userRoute);
 app.use("/api/items/", itemsRoute);
 app.use("/api/orders/", ordersRoute);
 
 const port = process.env.PORT || 5000;
+
+if(process.env.NODE_ENV === "production"){
+  app.use(express.static(path.join(__dirname, "client/build")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client","build","index.html"))
+  })
+}
 
 app.listen(port, () => "Server Running on port " + port);
